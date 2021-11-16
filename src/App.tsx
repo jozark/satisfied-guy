88d91a1329b131031dbf20e5e868b1fa464b208d
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-11-16 20:38:09 +0100

    ✨ Use Header in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from 'styled-components';
 import { theme } from './components/styles/theme';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { GlobalStyles } from './components/styles/Global';
+import Header from './components/Header';
 
 function App(): JSX.Element {
   return (
@@ -9,7 +10,7 @@ function App(): JSX.Element {
       <GlobalStyles />
       <Router>
         <Routes>
-          <Route path="/" element={<body>Wello Horld</body>}></Route>
+          <Route path="/" element={<Header />}></Route>
         </Routes>
       </Router>
     </ThemeProvider>

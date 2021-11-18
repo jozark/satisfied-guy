6f290833da664f37bcee8fff0c2dc804f67235a0
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-11-18 20:52:28 +0100

    💄 Change behavior of Hero

--- src/components/styles/Hero.styled.ts
+++ src/components/styles/Hero.styled.ts
@@ -15,13 +15,28 @@ export const Video = styled.video`
 export const ImageLeft = styled.img`
   margin-left: -8%;
   width: 100%;
+  min-width: 580px;
   padding-bottom: 10%;
+
+  @media (max-width: ${({ theme }) => theme.breakingPoints.mobile}) {
+    min-width: 300px;
+    width: 500px;
+    padding-bottom: 0;
+  }
 `;
 
 export const ImageRight = styled.img`
   justify-self: flex-end;
   width: 30%;
+  min-width: 140px;
   margin-right: 20%;
+  @media (max-width: ${({ theme }) => theme.breakingPoints.mobile}) {
+    width: 140px;
+    padding-bottom: 2rem;
+    margin-right: 0;
+    margin-left: 4rem;
+    justify-self: center;
+  }
 `;
 
 export const Grid = styled.div`
@@ -32,4 +47,10 @@ export const Grid = styled.div`
   gap: 1rem;
   grid-auto-flow: column;
   align-items: flex-end;
+
+  @media (max-width: ${({ theme }) => theme.breakingPoints.mobile}) {
+    grid-template-columns: auto;
+    grid-auto-flow: row;
+    grid-template-rows: 1fr 1fr;
+  }
 `;

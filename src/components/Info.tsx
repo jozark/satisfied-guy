a125142ae27a14eb60fc3b3b7c73e87cfb84f0bd
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-11-18 21:18:39 +0100

    ✨ Create grid structure on info.tsx

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
-import { StyledInfo } from './styles/Info.styled';
+import { Container } from './styles/Container.styled';
+import { Image, StyledInfo } from './styles/Info.styled';
 
 export default function Info(): JSX.Element {
   return (
-    <StyledInfo>
-      <h1>Hallo</h1>
-    </StyledInfo>
+    <Container>
+      <StyledInfo>
+        <Image src="images/double_face.png" alt="" />
+        <div>
+          <h1>Satisfied Guy</h1>
+          <h2>Street Art Goes Digital</h2>
+          <p>
+            A local street artist from Hamburg created 5000 unique pictures of
+            his signature artwork. He is represented by an emerging art gallery
+            from Hamburg. A local street artist from Hamburg created 5000 unique
+            pictures of his signature artwork. He is represented by an emerging
+            art gallery from Hamburg.
+          </p>
+          <p>
+            A local street artist from Hamburg created 5000 unique pictures of
+            his signature artwork. He is represented by an emerging art gallery
+            from Hamburg. A local street artist from Hamburg created 5000 unique
+            pictures of his signature artwork. He is represented by an emerging
+            art gallery from Hamburg.
+          </p>
+        </div>
+      </StyledInfo>
+    </Container>
   );
 }
--- src/components/styles/Info.styled.ts
+++ src/components/styles/Info.styled.ts
@@ -1,7 +1,19 @@
 import styled from 'styled-components';
 
 export const StyledInfo = styled.div`
-  height: 100vh;
   display: grid;
+  min-height: 100vh;
+  padding-top: 3rem;
+  grid-template-columns: 1fr 1fr;
   place-items: center;
+  gap: 3rem;
+
+  @media (max-width: ${({ theme }) => theme.breakingPoints.tablet.max}) {
+    grid-template-columns: auto;
+  }
+`;
+
+export const Image = styled.img`
+  width: 500px;
+  height: auto;
 `;

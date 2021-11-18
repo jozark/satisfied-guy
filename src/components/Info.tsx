79dcf2f2ee0d1eb7cc5b81943c29aa4733467a62
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-11-18 12:59:33 +0100

    ✨ Create Basic Info Element

---
+++ src/components/Info.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyledInfo } from './styles/Info.styled';
+
+export default function Info(): JSX.Element {
+  return (
+    <StyledInfo>
+      <h1>Hallo</h1>
+    </StyledInfo>
+  );
+}

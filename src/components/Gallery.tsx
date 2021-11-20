b74d6eda24683dbbc327c422b741280eae5f94e2
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-11-20 17:05:50 +0100

    🐛 Add overflow scroll in Gallery

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -30,6 +30,26 @@ export default function Gallery(): JSX.Element {
           street="Schulterblatt"
           number="05"
         />
+        <GalleryElement
+          path="images/one.jpg"
+          street="Schulterblatt"
+          number="05"
+        />
+        <GalleryElement
+          path="images/two.jpg"
+          street="Schulterblatt"
+          number="05"
+        />
+        <GalleryElement
+          path="images/three.jpg"
+          street="Schulterblatt"
+          number="05"
+        />
+        <GalleryElement
+          path="images/four.jpg"
+          street="Schulterblatt"
+          number="05"
+        />
       </GalleryWrapper>
     </StyledGallery>
   );
--- src/components/styles/Gallery.styled.ts
+++ src/components/styles/Gallery.styled.ts
@@ -5,6 +5,8 @@ export const StyledGallery = styled.div`
   align-items: center;
   width: 100%;
   height: 100vh;
+  overflow: scroll;
+  padding: 0 2rem;
 `;
 
 export const GalleryWrapper = styled.div`

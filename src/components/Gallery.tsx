359d0df3fab24f54d5c9ea2ee66e53a83d891019
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-11-20 15:53:09 +0100

    ✨ Create basic structure of Gallery

---
+++ src/components/Gallery.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyledGallery } from './styles/Gallery.styled';
+
+type GalleryProps = {
+  img: string[];
+};
+
+export default function Gallery(): JSX.Element {
+  return <StyledGallery>hallos</StyledGallery>;
+}

1039627537e7a11494c6457f4631f249b2fed125
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-11-20 15:04:42 +0100

    ✨ Finalize Info

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container } from './styles/Container.styled';
-import { Image, StyledInfo } from './styles/Info.styled';
+import { Image, StyledInfo, Heading, Subheading } from './styles/Info.styled';
 
 export default function Info(): JSX.Element {
   return (
@@ -8,21 +8,23 @@ export default function Info(): JSX.Element {
       <StyledInfo>
         <Image src="images/double_face.png" alt="" />
         <div>
-          <h1>Satisfied Guy</h1>
-          <h2>Street Art Goes Digital</h2>
+          <Heading>Satisfied Guy</Heading>
+          <Subheading>Street Art Goes Digital</Subheading>
           <p>
-            A local street artist from Hamburg created 5000 unique pictures of
-            his signature artwork. He is represented by an emerging art gallery
-            from Hamburg. A local street artist from Hamburg created 5000 unique
-            pictures of his signature artwork. He is represented by an emerging
-            art gallery from Hamburg.
+            A local street artist from Hamburg created 500 unique handdrawn
+            pieces of his signature artwork. He is represented by an emerging
+            art gallery from Hamburg. His artwork can be found on almost every
+            corner in Hamburg's hip neighborhoods like the Schanzenviertel,
+            Karoviertel and St. Pauli.
           </p>
           <p>
-            A local street artist from Hamburg created 5000 unique pictures of
-            his signature artwork. He is represented by an emerging art gallery
-            from Hamburg. A local street artist from Hamburg created 5000 unique
-            pictures of his signature artwork. He is represented by an emerging
-            art gallery from Hamburg.
+            Sometimes the street artists act on the border of what's allowed.
+            They take the potential risk of paying high fines. Or even worse,
+            getting arrested. However, their work also contributes to the
+            culture and uniqueness of cities. Usually, street artists need to
+            stay anonym and make a living elsewhere. NFTs finally give street
+            artists the opportunity to build a community and interact with it in
+            a meaningful way.
           </p>
         </div>
       </StyledInfo>

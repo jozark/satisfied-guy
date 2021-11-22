73a04027147613436cc1b45544a30acc8671ae45
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-11-22 18:45:29 +0100

    ✨ Create GalleryImages

---
+++ src/components/assets/GalleryImages.ts
@@ -0,0 +1,37 @@
+export const GalleryImages = [
+  {
+    path: 'images/one.jpg',
+    street: 'Schulterblatt',
+    number: '05',
+  },
+  {
+    path: 'images/two.jpg',
+    street: 'Schulterblatt',
+    number: '05',
+  },
+  {
+    path: 'images/three.jpg',
+    street: 'Schulterblatt',
+    number: '05',
+  },
+  {
+    path: 'images/four.jpg',
+    street: 'Schulterblatt',
+    number: '05',
+  },
+  {
+    path: 'images/one.jpg',
+    street: 'Schulterblatt',
+    number: '05',
+  },
+  {
+    path: 'images/two.jpg',
+    street: 'Schulterblatt',
+    number: '05',
+  },
+  {
+    path: 'images/three.jpg',
+    street: 'Schulterblatt',
+    number: '05',
+  },
+];

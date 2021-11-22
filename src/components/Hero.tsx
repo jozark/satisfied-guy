40402a85850788e3e0e82ad9bce8a95e91690815
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-11-22 19:17:00 +0100

    ✨ Change Hero to Image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import {
-  StyledHero,
-  Video,
-  ImageLeft,
-  ImageRight,
-  Grid,
-} from './styles/Hero.styled';
+import { StyledHero, ImageLeft, ImageRight, Grid } from './styles/Hero.styled';
 
 export default function Hero(): JSX.Element {
   return (
     <StyledHero>
-      <Video src="videos/snoff.mp4" autoPlay loop muted />
+      {/* <Video src="videos/snoff.mp4" autoPlay loop muted /> */}
       <Grid>
         <ImageLeft src="images/calligraphy.png" alt="" />
         <ImageRight src="images/hero_face.png" alt="" />
